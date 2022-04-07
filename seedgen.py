627417188b6bcedf01088b6ffe110a1d176a0d68
greyhound <63813294+a6thmfsin@users.noreply.github.com>
2022-04-07 14:17:54 +0300

    Add files via upload

---
+++ seedgen.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import string    
+import random
+import threading
+
+def my_function():
+    f= open("seeds.txt","w+")
+    for i in range(99999):
+        S = 15    
+        ran = ''.join(random.choices(string.ascii_lowercase + string.digits, k = S))  
+        #print(str(ran))
+        f.write("\n" + (str(ran)))
+    
+my_function()
+
+x = threading.Thread(target=my_function)
+x.start()
+
+#Seed.gen made for MediaFire fishing with wfuzz by greyhound, S is for how many letters/numbers will be generated.
+#Range is for how many seeds will be generated.
+#wfuzz -c -w seeds.txt --hc=404 https://www.mediafire.com/file/FUZZ
